5befb2f2610c8a069246e77b44d47759f1067098
EvteevAleksey <39763484+EvteevAleksey@users.noreply.github.com>
2018-05-30 14:15:30 +0400

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,24 @@
+import telebot
+import sys
+import time
+from telebot import apihelper
+
+apihelper.proxy = {'http':'http://195.201.137.246:1080'}
+TOKEN = '529413272:AAGPFw8xASCkvhY-9rqhqtVAmIFaRnrb0go'
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    sent = bot.send_message(message.chat.id, 'Как тебя зовут?')
+    bot.register_next_step_handler(sent, hello)
+
+def hello(message):
+    bot.send_message(message.chat.id, 'Привет, {name}. Рад тебя видеть.'.format(name=message.text))
+
+while True:
+        try:
+            bot.polling(none_stop=True)
+        except Exception as e:
+            print('Error occurred:')
+            print(sys.stderr, str(e))
+            time.sleep(10)
